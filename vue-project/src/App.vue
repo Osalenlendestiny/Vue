829a47caad09ebe7cd90dbf0e51970734cebd7fa
Osalenlendestiny <graphixtribe360@gmail.com>
2023-07-16 19:02:16 +0100

    shopping list

---
+++ vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const text= ref ('Shopping list')
+    const show = ref(true)
+    const list = ref([1, 2, 3])
+</script>
+
+
+<template>
+    <h1> {{ text }}</h1>
+
+
+    <button @click="show = !show">New item</button>
+    
+
+</template>
